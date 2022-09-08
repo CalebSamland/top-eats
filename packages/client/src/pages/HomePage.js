72061d88e1c2d5ae5b742462746ff8bd5b896b76
Caleb Samland <cjsamland@gmail.com>
2022-09-08 13:12:54 -0400

    Added placeholder form and divs for components

--- packages/client/src/pages/HomePage.js
+++ packages/client/src/pages/HomePage.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
+  return (<>
+
+  {/* The header will be a separate component */}
+    <header>
+      <h1>Top Eats</h1>
+      <nav>Login / Signup / Profile Image</nav>
+    </header>
+
+{/* The search bar will be a separate component */}
+    <form>
+      <label>
+        Search Cuisine
+        <input type='text' />
+      </label>
+      <label>
+        Search Location
+        <input type='text' />  
+      </label>
+    </form>
+  {/* The restaurantList and individual restaurantPreview will be separant components */}
+    <div>
+      <h2>Restaurant List</h2>
+      {/* Map through the restaurants and render individual restaurantPreview components */}
+      <div>Restaurant Name, Location, Website Link</div>
+    </div>
+
+  
+  </>)
 }
 
 export default HomePage
