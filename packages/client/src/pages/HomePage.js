d2fc1672638adcec212c96bedbb8765d380c1a79
J-David-Baxter <jdavidbx@gmail.com>
2022-09-08 19:45:41 -0400

    Render SearchBar in updated homepage

--- packages/client/src/pages/HomePage.js
+++ packages/client/src/pages/HomePage.js
@@ -1,13 +1,26 @@
 import React from 'react'
-import SearchBar from '../components/SearchBar/SearchBar'
+import SearchBar from '../components/SearchBar/SearchBar';
 
 const HomePage = () => {
-  return (
-    <>
-        <div>HomePage</div>
-        <SearchBar />
-    </>
-  )
+  return (<>
+
+  {/* The header will be a separate component */}
+    <header>
+      <h1>Top Eats</h1>
+      <nav>Login / Signup / Profile Image</nav>
+    </header>
+
+{/* The search bar will be a separate component */}
+    <SearchBar />
+  {/* The restaurantList and individual restaurantPreview will be separant components */}
+    <div>
+      <h2>Restaurant List</h2>
+      {/* Map through the restaurants and render individual restaurantPreview components */}
+      <div>Restaurant Name, Location, Website Link</div>
+    </div>
+
+  
+  </>)
 }
 
 export default HomePage
