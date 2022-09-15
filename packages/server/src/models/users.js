d1b94b149ffb99ba6de82e101459dd25855d2858
Jordan Barone <j.barone024@gmail.com>
2022-09-15 13:41:48 -0400

    User Model

---
+++ packages/server/src/models/users.js
@@ -0,0 +1,56 @@
+import mongoose, { Mongoose } from "mongoose";
+const { ObjectId } = mongoose.Schema.Types;
+
+const userSchema = new Mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    zip: {
+      type: Number,
+      unique: true,
+      required: true,
+    },
+    profile_image: {
+      type: String,
+      required: false,
+    },
+    birthday: {
+      type: String,
+      required: false,
+    },
+    reviews: [
+      {
+        type: ObjectId,
+      },
+    ],
+    savedRestaurants: [
+      {
+        type: ObjectId,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
