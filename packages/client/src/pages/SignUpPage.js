2b04dda60a375dac1d51bcbb5208fcb6764515b8
J-David-Baxter <jdavidbx@gmail.com>
2022-09-15 15:54:33 -0400

    Complete styles, inputs still need centering

--- packages/client/src/pages/SignUpPage.js
+++ packages/client/src/pages/SignUpPage.js
@@ -12,6 +12,7 @@ import Header from '../components/Header/Header';
 const initialState = {
     firstName: '',
     lastName: '',
+    birthday: '',
     zip: '',
     email: '',
     password: '',
@@ -29,35 +30,52 @@ const SignUpPage = () => {
         })
     }
 
+    const handleSubmit = e => {
+        // Make POST request to API to store form data in database
+    }
+
     return (
-        <>
+        <Container>
             <h3 style={{textAlign: 'center'}}>Sign Up</h3>
             <Form style={{display: 'flex', flexDirection: 'column'}}>
                 <Form.Group className="mb-3" style={{margin: '10px'}}>
                     <Form.Label>First Name</Form.Label>
-                    <Form.Control type="text" name="firstName" value={data.firstName} onChange={handleChange} />
+                    <Form.Control type="text" name="firstName" value={data.firstName} onChange={handleChange} required/>
                 </Form.Group>
                 <Form.Group className="mb-3" style={{margin: '10px'}}>
                     <Form.Label>Last Name</Form.Label>
-                    <Form.Control type="text" name="lastName" value={data.lastName} onChange={handleChange} />
+                    <Form.Control type="text" name="lastName" value={data.lastName} onChange={handleChange} required/>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Birthday (Optional)</Form.Label>
+                    <Form.Control type="text" name="birthday" value={data.birthday} onChange={handleChange} />
                 </Form.Group>
                 <Form.Group className="mb-3" style={{margin: '10px'}}>
                     <Form.Label>Zip Code</Form.Label>
-                    <Form.Control type="text" name="zip" value={data.zip} onChange={handleChange} />
+                    <Form.Control type="text" name="zip" value={data.zip} onChange={handleChange} required/>
                 </Form.Group>
                 <Form.Group className="mb-3" style={{margin: '10px'}}>
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" name="email" value={data.email} onChange={handleChange} />
+                    <Form.Control type="email" name="email" value={data.email} onChange={handleChange} required/>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Confirm Email</Form.Label>
+                    <Form.Control type="email" name="confirmEmail" value={data.confirmEmail} onChange={handleChange} required/>
                 </Form.Group>
                 <Form.Group className="mb-3" style={{margin: '10px'}}>
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name="password" value={data.password} onChange={handleChange} />
+                    <Form.Control type="password" name="password" value={data.password} onChange={handleChange} required/>
                 </Form.Group>
-                <Button variant="primary" type="submit">
+                <Form.Group>
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control type="password" name="confirmPassword" value={data.confirmPassword} onChange={handleChange} required/>
+                </Form.Group>
+                {data.password !== data.confirmPassword || data.email !== data.confirmEmail ? <span style={{color: 'red'}}>Email or Password Do Not Match</span> : <span></span>}
+                <Button variant="primary" type="submit" onClick={handleSubmit}>
                     Submit
                 </Button>
             </Form>
-        </>
+        </Container>
     )
 }
 
