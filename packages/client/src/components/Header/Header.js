f2f8c63ab1f655879f9bc00a061eaf8fadce179a
Caleb Samland <cjsamland@gmail.com>
2022-09-22 14:49:36 -0400

    Rerouted pages / header to toggle user

--- packages/client/src/components/Header/Header.js
+++ packages/client/src/components/Header/Header.js
@@ -2,33 +2,39 @@ import React, { useState } from 'react'
 import { Button, Container, Image, Navbar, Nav, Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
-const Header = () => {
+const Header = ({ user, setUser }) => {
 
-  const [user, setUser] = useState(
-    {
-      firstName: 'Bob',
-      lastName: 'Smith',
-      userName: 'bobster'
-    })
+  console.log({user})
+
+
+
+  // const [user, setUser] = useState(
+  //   {
+  //     firstName: 'Bob',
+  //     lastName: 'Smith',
+  //     userName: 'bobster'
+  //   })
 
   const logout = () => {
     setUser(null)
   }
+
+
 
   return (
     <Navbar bg='secondary' variant='dark'>
       <Container fluid='lg' style={{maxWidth: '1024px'}}>
         <Navbar.Brand as={Link} to='/'>Top Eats</Navbar.Brand>
         {
-          user ?
+          (user === false) ?
           <Nav className='justify-content-end'>
               <Nav.Link onClick={()=>logout()}>Logout</Nav.Link>      
               <Image />
             </Nav>
           :
           <Nav>
-              <Nav.Link>Login</Nav.Link>
-              <Nav.Link>Sign Up</Nav.Link>
+              <Nav.Link><Link to={`/signin`}>Login</Link></Nav.Link>
+              <Nav.Link><Link to={`/signup`}>Sign Up</Link></Nav.Link>
             </Nav>  
         }
       </Container>
