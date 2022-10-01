c5d33dab69be2d6785d2365bd15b084368841d39
J-David-Baxter <jdavidbx@gmail.com>
2022-10-01 14:51:15 -0400

    Add sessionstorage hook

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -7,9 +7,10 @@ import SignUpPage from "./pages/SignUpPage";
 import SignInPage from "./pages/SignInPage";
 import UserProfile from "./pages/UserProfile";
 import {useLocalStorage} from "./hooks/useLocalStorage";
+import {useSessionStorage} from "./hooks/useSessionStorage";
 
 function App() {
-  const [restaurants, setRestaurants] = useLocalStorage("restaurants", null);
+  const [restaurants, setRestaurants] = useSessionStorage("restaurants", null);
   const [user, setUser] = useLocalStorage("user", null);
 
   return (
