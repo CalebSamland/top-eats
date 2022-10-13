5838450f85fdbf56e8171e235bacd3a1edca3e9f
Caleb Samland <cjsamland@gmail.com>
2022-10-13 11:36:04 -0400

    added link to html page on server

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -5,6 +5,8 @@ import mongoose from 'mongoose';
 import { API_URL, DB_URL } from './configs/index.js';
 import routes from './routes/index.js';
 
+import * as path from 'path'
+
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -17,5 +19,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(API_URL, routes);
+
+// **Some imports .... **
+
+
+// ................ 
+// Other Routes and Code...
+// ...............
+// (Add below code after all other routes)
+//Serve Static Assets in production 
+//set static folder
+
+app.use(express.static('/client/build'));
+app.get('*', (req, res) => {
+res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));});
 
 export default app;
