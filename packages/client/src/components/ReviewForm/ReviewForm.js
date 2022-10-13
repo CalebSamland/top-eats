51a91f90d06a4644d4df186f764d23e49837ff6b
Jordan Barone <j.barone024@gmail.com>
2022-10-13 18:10:23 -0400

    Toast

--- packages/client/src/components/ReviewForm/ReviewForm.js
+++ packages/client/src/components/ReviewForm/ReviewForm.js
@@ -80,37 +80,35 @@ const ReviewForm = ({ restaurant, user, reviews, setReviews }) => {
       // errorMessage: null,
     });
     // }
-    console.log(reviewData)
-    console.log(rating)
-    axios
-      .post('http://localhost:3001/api/newReview', reviewData)
-      .then(
-        (res) => {
-          setReviewData(initialState)
-          setReviews(() => [
-            ...reviews.reverse(),
-            {
-                reviewBody: reviewData.text,
-                rating: reviewData.rating,
-                author: reviewData.userID,
-                restaurantID: reviewData.restaurantID,
-                createdAt: new Date()
-            },
-          ])
-          setReviewData(initialState);
-          setRating(0);
-          setHover(0);
-        },
-        (error) => {
-          console.log(error)
-          setReviewData({
-            ...reviewData,
-            // isSubmitting: false,
-            // errorMessage: error.message,
-          })
-        }
-      )
-  }
+    console.log(reviewData);
+    console.log(rating);
+    axios.post("http://localhost:3001/api/newReview", reviewData).then(
+      (res) => {
+        setReviewData(initialState);
+        setReviews(() => [
+          ...reviews.reverse(),
+          {
+            reviewBody: reviewData.text,
+            rating: reviewData.rating,
+            author: reviewData.userID,
+            restaurantID: reviewData.restaurantID,
+            createdAt: new Date(),
+          },
+        ]);
+        setReviewData(initialState);
+        setRating(0);
+        setHover(0);
+      },
+      (error) => {
+        console.log(error);
+        setReviewData({
+          ...reviewData,
+          // isSubmitting: false,
+          // errorMessage: error.message,
+        });
+      }
+    );
+  };
   // useEffect(() => {
   //   const getReviews = async () => {
   //     try {
@@ -145,54 +143,54 @@ const ReviewForm = ({ restaurant, user, reviews, setReviews }) => {
 
   return (
     <>
-        <Card style={{ padding: "25px", marginTop: "30px" }}>
-          <Card.Title style={{ fontSize: "24px" }}>Write a Review</Card.Title>
-          <Card.Body style={{ marginLeft: "-15px" }}>
-            <Form onSubmit={handleReviewSubmit}>
-              <div
-                className="star-rating"
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  width: "100px",
-                  marginBottom: "20px",
-                  marginTop: "-30px",
-                }}
-                required
-              >
-                {starsArray.map((star, idx) => {
-                  idx += 1;
-                  return (
-                    <button
-                      type="button"
-                      key={idx}
-                      className={
-                        idx <= ((rating && hover) || hover) ? "on" : "off"
-                      }
-                      onClick={() => setRating(idx)}
-                      onMouseEnter={() => setHover(idx)}
-                      onMouseLeave={() => setHover(rating)}
-                      onDoubleClick={() => {
-                        setRating(0);
-                        setHover(0);
-                      }}
-                    >
-                      <div className="star">&#9733;</div>
-                    </button>
-                  );
-                })}
-              </div>
-              <FloatingLabel label="Your Review Here">
-                <FormControl
-                  // as="textarea"
-                  placeholder="Your review here."
-                  style={{ height: "150px", width: '50vw', maxWidth: '900px'}}
-                  name="text"
-                  value={reviewData.text}
-                  onChange={handleInputChange}
-                ></FormControl>
-              </FloatingLabel>
-              {/* <Button
+      <Card style={{ padding: "25px", marginTop: "30px" }}>
+        <Card.Title style={{ fontSize: "24px" }}>Write a Review</Card.Title>
+        <Card.Body style={{ marginLeft: "-15px" }}>
+          <Form onSubmit={handleReviewSubmit}>
+            <div
+              className="star-rating"
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                width: "100px",
+                marginBottom: "20px",
+                marginTop: "-30px",
+              }}
+              required
+            >
+              {starsArray.map((star, idx) => {
+                idx += 1;
+                return (
+                  <button
+                    type="button"
+                    key={idx}
+                    className={
+                      idx <= ((rating && hover) || hover) ? "on" : "off"
+                    }
+                    onClick={() => setRating(idx)}
+                    onMouseEnter={() => setHover(idx)}
+                    onMouseLeave={() => setHover(rating)}
+                    onDoubleClick={() => {
+                      setRating(0);
+                      setHover(0);
+                    }}
+                  >
+                    <div className="star">&#9733;</div>
+                  </button>
+                );
+              })}
+            </div>
+            <FloatingLabel label="Your Review Here">
+              <FormControl
+                // as="textarea"
+                placeholder="Your review here."
+                style={{ height: "150px", width: "50vw", maxWidth: "900px" }}
+                name="text"
+                value={reviewData.text}
+                onChange={handleInputChange}
+              ></FormControl>
+            </FloatingLabel>
+            {/* <Button
             variant="info"
             style={{
               width: "125px",
@@ -203,23 +201,22 @@ const ReviewForm = ({ restaurant, user, reviews, setReviews }) => {
             >
             Add Photos
           </Button> */}
-              <Button
-                type="submit"
-                variant="success"
-                style={{
-                  width: "125px",
-                  height: "40px",
-                  fontSize: "14px",
-                  marginTop: "10px",
-                  float: "right",
-                }}
-              >
-                Submit
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
-      
+            <Button
+              type="submit"
+              variant="success"
+              style={{
+                width: "125px",
+                height: "40px",
+                fontSize: "14px",
+                marginTop: "10px",
+                float: "right",
+              }}
+            >
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
       <ToastContainer
         position="top-right"
         autoClose={5000}
