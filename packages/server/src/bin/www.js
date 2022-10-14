3153dd807d8c2f09d233f81490be1f0cff438af0
Caleb Samland <cjsamland@gmail.com>
2022-10-14 14:11:04 -0400

    cleaning up the server code

--- packages/server/src/bin/www.js
+++ packages/server/src/bin/www.js
@@ -4,54 +4,53 @@
  * Module dependencies.
  */
 
-import app from '../app.js'
+import app from "../app.js";
 // import debug from "server";
 import http from "http";
-import chalk from "chalk"
+import chalk from "chalk";
 import "core-js/stable/index.js";
 import "regenerator-runtime";
 import { PORT } from "../configs/index.js";
-
 
 /**
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(PORT)
-app.set('port', port)
+const port = normalizePort(PORT);
+app.set("port", port);
 
 /**
  * Create HTTP server.
  */
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.
  */
 
-server.listen(port)
-server.on('error', onError)
-server.on('listening', onListening)
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
 
 /**
  * Normalize a port into a number, string, or false.
  */
 
 function normalizePort(val) {
-  const port = parseInt(val, 10)
+  const port = parseInt(val, 10);
 
   if (isNaN(port)) {
     // named pipe
-    return val
+    return val;
   }
 
   if (port >= 0) {
     // port number
-    return port
+    return port;
   }
 
-  return false
+  return false;
 }
 
 /**
@@ -59,24 +58,24 @@ function normalizePort(val) {
  */
 
 function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error
+  if (error.syscall !== "listen") {
+    throw error;
   }
 
-  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges')
-      process.exit(1)
-      break
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use')
-      process.exit(1)
-      break
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
     default:
-      throw error
+      throw error;
   }
 }
 
@@ -85,11 +84,10 @@ function onError(error) {
  */
 
 function onListening() {
-
   console.log(
-    `${chalk.green('✓')} ${chalk.blue(
+    `${chalk.green("✓")} ${chalk.blue(
       `Listening on port ${port}. Visit http://localhost:${port}/ in your browser.`
       // "http://18.223.97.130/api/restaurants" for the deployed server
     )}`
-  )
+  );
 }
